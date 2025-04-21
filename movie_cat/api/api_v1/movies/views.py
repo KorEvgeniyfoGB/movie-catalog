611832faf07a3966cc560e36b0b12e5925d27948
Evgeniy <you@example.com>
2025-04-21 18:22:50 +0300

    view:create Movie with Form

--- movie_cat/api/api_v1/movies/views.py
+++ movie_cat/api/api_v1/movies/views.py
@@ -1,6 +1,12 @@
+from random import randint
 from typing import Annotated
 
-from fastapi import Depends, APIRouter
+from fastapi import (
+    Depends,
+    APIRouter,
+    Form,
+)
+from starlette import status
 from starlette.responses import RedirectResponse
 
 from api.api_v1.movies.dependencies import prefetch_movie_by_id
@@ -10,13 +16,39 @@ from schemas.movie import Movie
 router = APIRouter(prefix="/movies", tags=["Movies"])
 
 
-@router.get("/", response_model=list[Movie])
+@router.get(
+    "/",
+    response_model=list[Movie],
+)
 def read_movies_list():
     return MOVIE_LIST
 
 
-@router.get("/{id}", response_model=Movie)
-def read_movie_by_id(movie: Annotated[Movie, Depends(prefetch_movie_by_id)]) -> Movie:
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(title: str):
+    return Movie(
+        id=randint(
+            1,
+            150,
+        ),
+        title=title,
+    )
+
+
+@router.get(
+    "/{id}",
+    response_model=Movie,
+)
+def read_movie_by_id(
+    movie: Annotated[
+        Movie,
+        Depends(prefetch_movie_by_id),
+    ],
+) -> Movie:
     return movie
 
 
