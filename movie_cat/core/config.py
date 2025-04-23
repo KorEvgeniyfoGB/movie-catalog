5fb0396da80a25cb7eac005820d2396c05237001
Evgeniy <Bagz.bann4-l011a.Bann4@yandex.by>
2025-04-23 20:00:27 +0300

    fix config

--- movie_cat/core/config.py
+++ movie_cat/core/config.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 MOVIE_STORAGE_FILE_PATH = BASE_DIR / "storage.json"
 
 
-# if __name__ == "__main__":
-#     print(MOVIE_STORAGE_FILE_PATH)
-#     print(IS_MOVIE_STORAGE_EXIST)
+if __name__ == "__main__":
+    print(BASE_DIR)
+    print(MOVIE_STORAGE_FILE_PATH)
